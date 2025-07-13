c1a3b3941fd669fadcb881f5351027492796437b
jin1963 <kaojino89@gmail.com>
2025-07-14 01:58:15 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+
+const CONFIG = {
+  tokenAddress: "0x6cfD8Fe423F20F94825b5edB1E94068fBea19dC9",
+  tokenSymbol: "G3X24",
+  tokenDecimals: 18,
+  tokenLogoURI: "https://jin1963.github.io/Kaojin/logo.png" // เปลี่ยนได้ภายหลังถ้าต้องการ
+};
